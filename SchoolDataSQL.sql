1dd8cb254910093cdf1d78c6888a281a7485b488
Fatih Samed Taylan <fatihsamedt@gmail.com>
2024-05-05 21:40:03 +0300

    Add files via upload

---
+++ SchoolDataSQL.sql
@@ -0,0 +1,18 @@
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_number VARCHAR(20) UNIQUE NOT NULL,
+    student_name VARCHAR(100) NOT NULL,
+	student_surname VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE courses (
+    course_code VARCHAR(20) PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE grades (
+    student_id INTEGER REFERENCES students(student_id),
+    course_code VARCHAR(20) REFERENCES courses(course_code),
+    grade NUMERIC(3, 2) NOT NULL,
+    PRIMARY KEY (student_id, course_code)
+);
